29de5865cff5f51ceb257e10dda022fc53fc9943
osmghira <os.mghira@gmail.com>
2024-11-07 19:38:37 +0100

    un msg

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Main{
     public static void main(String [] args){
-        System.out.println("en cours de developpement");
+        System.out.println("en cours de developpement , le temps est fatigue");
 
     }
 }
